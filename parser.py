77d9cf1a64fda19f892d4f8cb4b0fbaf3266d744
mevey <muthoni90@gmail.com>
2017-10-26 19:23:06 -0700

    running logreg

--- parser.py
+++ parser.py
@@ -1,9 +1,10 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 import xml.etree.ElementTree as ET
 import os
-
-#splits each document into individual words
-tokenize = lambda doc: doc.lower().split(" ")
 
 def read_files():
     """
@@ -17,9 +18,23 @@ def read_files():
         yield (tree.find('mainText').text, tree.find('veracity').text)
 
 
-documents = [f[0] for f in read_files() if f[0] is not None]
 
+documents = [f[0] for f in read_files() if f[0] is not None]
+possibilities = ['mixture of true and false', 'mostly false', 'no factual content', 'mostly true']
+predictions = [possibilities.index(f[1]) for f in read_files() if f[0] is not None]
+
+#Calculating TDIDF with Scikitlearn is much faster
 sklearn_tfidf = TfidfVectorizer(norm='l2',min_df=0, use_idf=True, smooth_idf=False, sublinear_tf=True, tokenizer=tokenize)
 sklearn_representation = sklearn_tfidf.fit_transform(documents)
 
-print(sklearn_representation)
+#splits data into training and test
+X_train, X_test, y_train, y_test = train_test_split(sklearn_representation, predictions, test_size = .3, random_state=25)
+LogReg = LogisticRegression()
+LogReg.fit(X_train, y_train)
+y_pred = LogReg.predict(X_test)
+print(y_pred)
+
+#Evaluate the model
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print(confusion_matrix)
+print(classification_report(y_test, y_pred))
