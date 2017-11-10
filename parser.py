c8145e997739df33afe00be5996fc061ee220ba8
Tim Burke <timothy.burke@berkeley.edu>
2017-11-10 11:45:11 -0800

    minor updates

--- parser.py
+++ parser.py
@@ -5,10 +5,12 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 import xml.etree.ElementTree as ET
 import os
+from gensim.models import Word2Vec
+
 
 def read_files():
     """
-    For each xml file return the main text and veracity
+    For each xml file return the main text and the veracity label
     """
     path = 'data/train/'
     for filename in os.listdir(path):
@@ -19,15 +21,17 @@ def read_files():
 
 tokenize = lambda doc: doc.lower().split(" ")
 
+
+
 documents = [f[0] for f in read_files() if f[0] is not None]
 possibilities = ['mixture of true and false', 'mostly false', 'no factual content', 'mostly true']
 predictions = [possibilities.index(f[1]) for f in read_files() if f[0] is not None]
 
-#Calculating TDIDF with Scikitlearn is much faster
+#Calculate TF-IDF over the main text of each article, creating vector representations of them
 sklearn_tfidf = TfidfVectorizer(norm='l2',min_df=0, use_idf=True, smooth_idf=False, sublinear_tf=True, tokenizer=tokenize)
 sklearn_representation = sklearn_tfidf.fit_transform(documents)
 
-#splits data into training and test
+# Splits data into training and test
 X_train, X_test, y_train, y_test = train_test_split(sklearn_representation, predictions, test_size = .3, random_state=25)
 LogReg = LogisticRegression()
 LogReg.fit(X_train, y_train)
@@ -35,6 +39,6 @@ y_pred = LogReg.predict(X_test)
 print(y_pred)
 
 #Evaluate the model
-confusion_matrix = confusion_matrix(y_test, y_pred)
-print(confusion_matrix)
-print(classification_report(y_test, y_pred))
+# confusion_matrix = confusion_matrix(y_test, y_pred)
+# print(confusion_matrix)
+# print(classification_report(y_test, y_pred))
