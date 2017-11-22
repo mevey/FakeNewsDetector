6a7c495c768b78bb976f668e27f30c8f81deec08
mevey <muthoni90@gmail.com>
2017-11-22 11:47:50 -0800

    logregcv

--- logreg.py
+++ logreg.py
@@ -1,6 +1,6 @@
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix, roc_auc_score
 import xml.etree.ElementTree as ET
@@ -24,25 +24,22 @@ features = feature_matrix([
 
 #Calculate TF-IDF over the main text of each article, creating vector representations of them
 tokenize = lambda doc: doc.lower().split(" ")
-sklearn_tfidf = TfidfVectorizer(norm='l2',min_df=0, use_idf=True, smooth_idf=False, sublinear_tf=True, tokenizer=tokenize, max_features=10000)
+sklearn_tfidf = TfidfVectorizer(norm='l2', sublinear_tf=True, tokenizer=tokenize, max_features=1000)
 sklearn_representation = sklearn_tfidf.fit_transform(documents)
+print("Done vectorizing")
 
 #input_ = sklearn_representation
-input_ = np.concatenate((sklearn_representation.todense() ,features), axis=1) #possible to array
+#input_ = np.concatenate((sklearn_representation.toarray() ,features), axis=1)
+input_ = sklearn_representation
 
 # Splits data into training and test
 X_train, X_test, y_train, y_test = train_test_split(input_, predictions, test_size = .3, random_state=25)
 
-#GridSearchCV
-params = {'C':[.00001,.0001,.001,.01,.1,.2,.5,.8,1,2,5,10,100]} # Dict of values to search over for regularization strength
-logreg = LogisticRegression(penalty='l2')
-best_logreg = GridSearchCV(logreg, params, scoring="f1_micro")
-best_logreg.fit(X_train,y_train)
-print(best_logreg.best_score_)
-logreg = best_logreg.best_estimator_
-print(logreg)
+logreg = LogisticRegressionCV(penalty='l2', scoring="roc_auc")
+logreg.fit(X_train,y_train)
 y_pred = logreg.predict(X_test)
 print(y_pred)
+print(logreg.score(X_test, y_test))
 
 #Evaluate the model
 """
@@ -51,4 +48,4 @@ print(confusion_matrix)
 classification_report(y_test, y_pred))
 """
 
-classification_report(y_test, y_pred)
+#print(classification_report(y_test, y_pred))
